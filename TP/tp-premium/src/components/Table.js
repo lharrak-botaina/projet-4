52b00bfae0fecefb5a7cd5f7b83fbd90090634fd
lharrak-botaina <lharrak.botaina@gmail.com>
2022-12-02 13:53:37 +0100

    [ADD]get data

--- TP/tp-premium/src/components/Table.js
+++ TP/tp-premium/src/components/Table.js
@@ -9,14 +9,15 @@ class Table extends React.Component{
             data:[]
         }
     }
-    componentDidMount(){
-        axios.get("http://127.0.0.1:8000/api/task")
+   async componentDidMount(){
+        await axios.get("http://127.0.0.1:8000/api/task")
         .then(res=>{
+            console.log(res.data)
             this.setState({
                 data:res.data
                
             })
-            console.log(data)
+            
         })
     }
 
